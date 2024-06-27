0d62e888d09c8c416d58edf2d4f9c35684c16c24
Vladimir <vovagarmonshikov@mail.ru>
2024-06-27 12:53:52 +0300

    last

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (_, res) => res.send('<h1>express one page</h1>'))
+app.get('/', (_, res) => res.send('<h1>express one page </h1>'))
 
-const port = 8080
+const port = 3000
 
 const server = app.listen(port, () => console.log(`Server has been started on port ${port}`))
